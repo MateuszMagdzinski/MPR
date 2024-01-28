3fc9b6937195c2ba4e14e1d0d3b8a24325f1bfaa
Mateusz Magdziński <magdzinski.mat@gmail.com>
2024-01-28 22:08:06 +0100

    dokończyć servis

---
+++ LAB2/src/main/java/com/example/LAB2/exception/CatExist.java
@@ -0,0 +1,5 @@
+package com.example.LAB2.exception;
+
+public class CatExist extends NullPointerException{
+    public CatExist(){super("Kot już istnieje");}
+}
---
+++ LAB2/src/main/java/com/example/LAB2/exception/CatNotExist.java
@@ -0,0 +1,5 @@
+package com.example.LAB2.exception;
+
+public class CatNotExist extends NullPointerException{
+    public CatNotExist(){super("Kot nie istnieje");}
+}
